a43949165fa500819fb71bde5f2e114eb65f8e14
Anto Ortega <antonellaportega@gmail.com>
2023-11-16 14:00:47 -0300

    ingresar saldo

--- Js/Ejercicio_2.js
+++ Js/Ejercicio_2.js
@@ -2,5 +2,16 @@ let cuenta = {
     titula: 'anto',
     saldo:0,
 
+    //ingresar
+
+    ingresar: function (cantidad){
+        if (cantidad>0){
+            this.saldo += cantidad;
+            alert('se ingreso ' + cantidad + 'Nuevo saldo: ' + this.saldo)
+        }else{
+            alert('la cantidad a ingresar debe mayor que 0');
+        }
+    },
+
     
 }
