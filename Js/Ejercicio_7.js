46d6e282dcdef5f9e2ead7668fb554aec6c0f87d
Anto Ortega <antonellaportega@gmail.com>
2023-11-16 15:03:57 -0300

    creacion de clase  contacto y agenda

---
+++ Js/Ejercicio_7.js
@@ -0,0 +1,17 @@
+class contacto{
+    constructor(nombre,telefono){
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+}
+
+//definir clase agenda
+
+class agenda{
+    constructor(tamano=10){
+        this.contacto = [];
+        this.tamano = tamano;
+    }
+
+    
+}
