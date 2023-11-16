590a7ec249d96b93c7ad3565c638ec46b3a31648
Anto Ortega <antonellaportega@gmail.com>
2023-11-16 14:53:04 -0300

    creamos clase libro

---
+++ Js/Ejercicio_6.js
@@ -0,0 +1,10 @@
+class libro {
+    constructor (ISBN,titulo,autor,paginas){
+        this._ISBN = ISBN;
+        this._titulo = titulo;
+        this._autor = autor;
+        this._paginas = paginas;
+    }
+
+    
+}
