bb9254d9904d729de9c8a8f124a52d313ce5ef3e
Anto Ortega <antonellaportega@gmail.com>
2023-11-16 14:13:02 -0300

    modificar alto

--- Js/Ejercicio_3.js
+++ Js/Ejercicio_3.js
@@ -3,4 +3,17 @@ class Rectangulo {
         this.alto = alto;
         this.ancho = ancho;
     }
+
+    //modificar el alto
+
+    modificarAlto(nuevoalto){
+        if(nuevoalto>0){
+            this.alto = nuevoalto;
+            alert('alto modificado a: ' + this.alto);
+        }else{
+            alert('el alto debe ser mayor que 0');
+        }
+    }
+
+    
 }
