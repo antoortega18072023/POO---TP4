0b8618156745e77f2e81ca70a3c8069b78a71599
Anto Ortega <antonellaportega@gmail.com>
2023-11-16 14:06:12 -0300

    informe de cuenta y correcion de sintaxis

--- Js/Ejercicio_2.js
+++ Js/Ejercicio_2.js
@@ -1,5 +1,5 @@
 let cuenta = {
-    titula: 'anto',
+    titular: 'Alex',
     saldo:0,
 
     //ingresar
@@ -7,7 +7,7 @@ let cuenta = {
     ingresar: function (cantidad){
         if (cantidad>0){
             this.saldo += cantidad;
-            alert('se ingreso ' + cantidad + 'Nuevo saldo: ' + this.saldo)
+            alert('se ingreso ' + cantidad + ' Nuevo saldo: ' + this.saldo);
         }else{
             alert('la cantidad a ingresar debe mayor que 0');
         }
@@ -18,11 +18,26 @@ let cuenta = {
     extraer: function(cantidad){
         if (cantidad>0 && cantidad<= this.saldo){
             this.saldo -=cantidad;
-            alert('se extrajeron ' + cantidad + 'Nuevo saldo: ' + this.saldo)
+            alert('se extrajeron ' + cantidad + 'Nuevo saldo: ' + this.saldo);
         }else{
-            alert('la cantidad a eztraer debe ser mayor que cero y no pude superar lo que hay en cuenta disponible')
+            alert('la cantidad a eztraer debe ser mayor que cero y no pude superar lo que hay en cuenta disponible');
         }
     },
 
-    
+    //estado de cuenra
+    informar: function(){
+        alert('Titular: ' + this.titular + ', Saldo actual: ' + this.saldo);
+    }
 }
+
+// Mostrar información inicial de la cuenta
+cuenta.informar();
+
+// Ingresar dinero en la cuenta
+cuenta.ingresar(100);
+
+// Extraer dinero de la cuenta
+cuenta.extraer(30);
+
+// Mostrar información actualizada de la cuenta
+cuenta.informar();
