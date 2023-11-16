58e27562660e4ce90fcda0183c8890be7b88dbb1
Anto Ortega <antonellaportega@gmail.com>
2023-11-16 15:09:48 -0300

    agregamos instancias

--- Js/Ejercicio_7.js
+++ Js/Ejercicio_7.js
@@ -13,5 +13,36 @@ class agenda{
         this.tamano = tamano;
     }
 
+    aniadirContacto(contacto) {
+        if (this.contactos.length < this.tamano) {
+          this.contactos.push(contacto);
+          console.log('Contacto añadido correctamente.');
+        } else {
+          console.log('La agenda está llena, no se puede añadir más contactos.');
+        }
+      }
+
+      existeContacto(contacto){
+        return this.contactos.some(c => c.nombre === contacto.nombre);
+      }
     
+      listarContactos() {
+        console.log('Lista de contactos:');
+        this.contactos.forEach(c => console.log(`${c.nombre}: ${c.telefono}`));
+      }
+      
+      buscarContacto(nombre) {
+        const contactoEncontrado = this.contactos.find(c => c.nombre === nombre);
+        if (contactoEncontrado) {
+          console.log(`Teléfono de ${contactoEncontrado.nombre}: ${contactoEncontrado.telefono}`);
+        } else {
+          console.log('Contacto no encontrado.');
+        }
+      }
 }
+
+//exixste contacto
+
+  
+
+
