e010fd6f5844e3ad410fffcec8618a339553aed8
Anto Ortega <antonellaportega@gmail.com>
2023-11-16 15:00:41 -0300

    compararcion de paginas

--- Js/Ejercicio_6.js
+++ Js/Ejercicio_6.js
@@ -39,3 +39,23 @@ class libro {
         this._paginas=paginas
     }
 }
+
+//instancias
+
+let libro1 = new libro ('123456', 'El señor de los anillos', 'J.R.R. Tolkien', 1000);
+let libro2 = new libro ('789012', 'Cien años de soledad', 'Gabriel García Márquez', 500);
+
+//mostrar
+
+libro1.mostrarlibro();
+libro2.mostrarlibro();
+
+//determianr mayor pagina
+
+if (libro1.paginas>libro2.paginas){
+    alert(`${libro1.titulo} tiene más páginas que ${libro2.titulo}.`);
+}else if(libro1.paginas<libro2.paginas){
+    alert(`${libro2.titulo} tiene más páginas que ${libro1.titulo}.`);
+}else{
+    alert(`${libro1.titulo} y ${libro2.titulo} tienen la misma cantidad de páginas.`);
+}
